ae900593cc27e2666963cb45d9db42547bc0d177
Apollo Tang <tang.apollo@gmail.com>
2017-04-21 10:56:17 -0400

    added babel-state-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import http from 'http'
 
 http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello');
+    const a = {a:'a'};
+    const b = {b:'b'};
+    const c = {...b};
+    const s = JSON.stringify(c);
+    console.log('hello again', s)
+    res.end('Hello again'+s);
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000/');
