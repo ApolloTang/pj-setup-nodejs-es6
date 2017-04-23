3693b7695b4d4d54aab76fa04914d6dc3df806d9
Apollo Tang <tang.apollo@gmail.com>
2017-04-23 15:45:21 -0400

    no need to import config in test

--- src/api/users/user.spec.js
+++ src/api/users/user.spec.js
@@ -4,9 +4,9 @@ import {expect} from 'chai';
 const ROOTPATH = process.env.ROOTPATH;
 console.log('ROOTPATH: ', ROOTPATH );
 
-const fullPathToConfig = ROOTPATH + '/src/config/index.js';
-const config = require(fullPathToConfig).default;
-console.log('user.spect.js config: ',  JSON.stringify(config, null, 4) );
+// const fullPathToConfig = ROOTPATH + '/src/config/index.js';
+// const config = require(fullPathToConfig).default;
+// console.log('user.spect.js config: ',  JSON.stringify(config, null, 4) );
 
 const fullPathToServer = ROOTPATH + '/src/server.js';
 const app = require(fullPathToServer).default;
