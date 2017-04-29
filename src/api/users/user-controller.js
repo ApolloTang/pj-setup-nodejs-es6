af97c15ab3db1b45b3e87c1f5bb0fec8d1e3f8e7
Apollo Tang <tang.apollo@gmail.com>
2017-04-28 20:51:01 -0400

    handling 404 user no found

--- src/api/users/user-controller.js
+++ src/api/users/user-controller.js
@@ -1,8 +1,11 @@
+import mongoose from 'mongoose';
 import User from './user-model';
 import _ from 'lodash';
 
-const params = (req, res, next, id) => {
-  User.findById(id)
+const params = (req, res, next, _id) => {
+  let id = _id
+  // id = mongoose.Types.ObjectId(_id);
+  User.findById(id).exec()
     .then(
       user=>{
         if (!user) {
@@ -12,7 +15,10 @@ const params = (req, res, next, id) => {
           next();
         }
       },
-      err=>{ next(err); }
+      err=>{
+        res.status(404).send(`${err.message}. Meaning: No user with id: ${id}`);
+        // next(err);
+      }
     );
 };
 
@@ -43,16 +49,21 @@ const get = (req, res, next) => {
 
 const getOne = (req, res, next) => {
   var user = req.user;
-  var id = user._id;
-  User.find({'_id':id})
-    .then(
-      foundUser => {
-        res.json(foundUser[0]);
-      },
-      err=>{
-        next(err);
-      }
-    );
+  res.json(user);
+  // -- The following is handle in param middleware -- //
+  //
+  // var user = req.user;
+  // var id = user._id;
+  // User.find({'_id':id})
+  //   .then(
+  //     foundUser => {
+  //       res.json(foundUser[0]);
+  //     },
+  //     err=>{
+  //       console.log('errorrrr: ', id)
+  //       res.status(404).send(`${err.message}`);
+  //     }
+  //   );
 };
 
 const put = (req, res, next) => {
