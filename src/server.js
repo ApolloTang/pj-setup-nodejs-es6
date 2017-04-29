3959a07f800ef29f9e6ccaaaa299756693a97013
Apollo Tang <tang.apollo@gmail.com>
2017-04-28 20:44:24 -0400

    use bluebird as promise

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
 import config from './config';
-import middleware from './middleware';
-import api from './api';
-import error from './middleware/error.js';
 
-const app = express();
-mongoose.Promise = global.Promise;
+
+// -------------- //
+// -- mongoose -- //
+// -------------- //
+
+// import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+const mongoose = bluebird.promisifyAll(require("mongoose"));
+
+// mongoose.Promise = global.Promise;
+mongoose.Promise = bluebird;
+
+// mongoose.set('error', false);
 
 
 // Wrap connect in try-catch to prevent error in testing resatart
@@ -19,10 +24,19 @@ try {
 }
 
 
+// ------------- //
+// -- express -- //
+// ------------- //
+
+import express from 'express';
+import middleware from './middleware';
+import api from './api';
+import error from './middleware/error.js';
+
+const app = express();
 middleware(app);
-
 app.use('/api', api);
-
 app.use(error());
+
 
 export default app;
