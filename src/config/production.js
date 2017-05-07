aec1d97fb0578a6d713718f13b88856d761f8c1e
Apollo Tang <tang.apollo@gmail.com>
2017-05-07 01:46:59 -0400

    [master] configuration for deployment

--- src/config/production.js
+++ src/config/production.js
@@ -1,4 +1,9 @@
 export default {
   // disbable logging for production
-  logging: false
+  logging: false,
+  db: {
+    url : process.env.MONGODB_URI || 'no db.url specified'
+    // url : 'testing'
+    // url: 'mongodb://apollotang:password@ds133251.mlab.com:33251/sushi'
+  }
 };
